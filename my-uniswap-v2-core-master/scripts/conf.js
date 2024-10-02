8905250d74a26ce170dac1aacd28d0d3b22a0158
nono_qihao <sqqihao@gmail.com>
2024-10-03 02:27:53 +0800

    Update conf.js

--- my-uniswap-v2-core-master/scripts/conf.js
+++ my-uniswap-v2-core-master/scripts/conf.js
@@ -1,4 +1,4 @@
-privateKey = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+privateKey = "xxxxxxxxxxxxxxxxxxxxxxxx";
 myWalletAddr = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 
 
